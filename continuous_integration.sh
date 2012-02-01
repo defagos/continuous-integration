97639e741980a7ec608256a71f32e9f509dfcd1c
Samuel Défago <defagos@gmail.com>
2012-02-01 14:22:09 +0100

    Creating continuous integration script

---
+++ continuous_integration.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# TODO: Extract SDKROOT from .pbxproj. Use it with xcodebuild (currently, the latest SDK is used)
+# TODO: Extract .xcodeproj files. Use it to build all projects and to create separate build logs for each of them
+# TODO: xcodebuild
+
+# Constants
+# TODO: Should be environment variables
+code_sign_identity="XXXXX"
+provisioning_profile="XXXXX"
+
+echo ""
+echo "--------------------------------------------------------------------------------------------------------------------------------------------------"
+echo "Continuous integration for $JOB_NAME, build $BUILD_NUMBER ($BUILD_ID)"
+echo "--------------------------------------------------------------------------------------------------------------------------------------------------"
+echo ""
+
+# Create a symbolic link from the workspace (which we can access using a URL) and the builds directory. This is where we will store our build logs
+# (this way, we can save them for each build number, and the files get deleted when a build is removed). If we had stored logs in the workspace 
+# directly, we would have lost them when the workspace is cleaned up). 
+buildlogs_dir="$WORKSPACE/buildlogs"
+if [ ! -e "$buildlogs_dir" ]; then
+    ln -s "$WORKSPACE/../builds/" "$buildlogs_dir"
+fi
+build_dir="$buildlogs_dir/$BUILD_NUMBER"
+
+# Simulator debug build
+echo "Building simulator Debug binaries..."
+echo "------------------------------------"
+echo "The full log is available under ${JOB_URL}ws/buildlogs/$BUILD_NUMBER/build_simulator_debug.log"
+xcodebuild clean build -configuration "Debug" -sdk iphonesimulator RUN_CLANG_STATIC_ANALYZER="true" &> "$build_dir/build_simulator_debug.log"
+if [ $? -ne "0" ]; then
+    echo "[STATUS] Build failed"
+    echo ""
+    exit 1  
+fi
+echo "[STATUS] Build succeeded"
+echo ""
+
+# Simulator release build
+echo "Building simulator Release binaries..."
+echo "--------------------------------------"
+echo "The full log is available under ${JOB_URL}ws/buildlogs/$BUILD_NUMBER/build_simulator_release.log"
+xcodebuild clean build -configuration "Release" -sdk iphonesimulator RUN_CLANG_STATIC_ANALYZER="true" &> "$build_dir/build_simulator_release.log"
+if [ $? -ne "0" ]; then
+    echo "[STATUS] Build failed"
+    echo ""
+    exit 1  
+fi
+echo "[STATUS] Build succeeded"
+echo ""
+
+# Device debug build
+echo "Building device Debug binaries..."
+echo "---------------------------------"
+echo "The full log is available under ${JOB_URL}ws/buildlogs/$BUILD_NUMBER/build_device_debug.log"
+xcodebuild clean build -configuration "Debug" -sdk iphoneos CODE_SIGN_IDENTITY="$code_sign_identity" PROVISIONING_PROFILE="$provisioning_profile" &> "$build_dir/build_device_debug.log"
+if [ $? -ne "0" ]; then
+    echo "[STATUS] Build failed"
+    echo ""
+    exit 1  
+fi
+echo "[STATUS] Build succeeded"
+echo ""
+
+# Device release build
+echo "Building device Release binaries..."
+echo "-----------------------------------"
+echo "The full log is available under ${JOB_URL}ws/buildlogs/$BUILD_NUMBER/build_device_release.log"
+xcodebuild clean build -configuration "Release" -sdk iphoneos CODE_SIGN_IDENTITY="$code_sign_identity" PROVISIONING_PROFILE="$provisioning_profile" &> "$build_dir/build_device_release.log"
+if [ $? -ne "0" ]; then
+    echo "[STATUS] Build failed"
+    echo ""
+    exit 1  
+fi
+echo "[STATUS] Build succeeded"
+echo ""
+
+# TODO: Create ipa
+# See http://stackoverflow.com/questions/2664885/xcode-build-and-archive-from-command-line
+# /usr/bin/xcrun -sdk iphoneos PackageApplication -v "${RELEASE_BUILDDIR}/${APPLICATION_NAME}.app" -o "${BUILD_HISTORY_DIR}/${APPLICATION_NAME}.ipa" --sign "${DEVELOPER_NAME}" --embed "${PROVISONING_PROFILE}"
+
+echo "[STATUS] End of continuous integration"
+

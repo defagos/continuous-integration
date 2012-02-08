851af1b2c480ab7db19337431afdf9764412fb73
Samuel Défago <defagos@gmail.com>
2012-02-08 16:25:01 +0100

    Using environment variables for code signing parameters

--- continuous_integration.sh
+++ continuous_integration.sh
@@ -2,12 +2,7 @@
 
 # TODO: Extract SDKROOT from .pbxproj. Use it with xcodebuild (currently, the latest SDK is used)
 # TODO: Extract .xcodeproj files. Use it to build all projects and to create separate build logs for each of them
-# TODO: xcodebuild
-
-# Constants
-# TODO: Should be environment variables
-code_sign_identity="XXXXX"
-provisioning_profile="XXXXX"
+# TODO: Document this script (most notably that it is meant to be used with Jenkins)
 
 echo ""
 echo "--------------------------------------------------------------------------------------------------------------------------------------------------"
@@ -23,6 +18,19 @@ if [ ! -e "$buildlogs_dir" ]; then
     ln -s "$WORKSPACE/../builds/" "$buildlogs_dir"
 fi
 build_dir="$buildlogs_dir/$BUILD_NUMBER"
+
+# Check required environment variables
+if [ -z "$CODE_SIGN_IDENTITY" ]; then
+    echo "[ERROR] The CODE_SIGN_IDENTITY environment variable must be set to the code signing identity to use"
+    echo ""
+    exit 1
+fi
+
+if [ -z "$PROVISIONING_PROFILE" ]; then
+    echo "[ERROR] The PROVISIONING_PROFILE environment variable must be set to the identififer of the provisioning profile to use"
+    echo ""
+    exit 1
+fi
 
 # Simulator debug build
 echo "Building simulator Debug binaries..."
@@ -54,7 +62,7 @@ echo ""
 echo "Building device Debug binaries..."
 echo "---------------------------------"
 echo "The full log is available under ${JOB_URL}ws/buildlogs/$BUILD_NUMBER/build_device_debug.log"
-xcodebuild clean build -configuration "Debug" -sdk iphoneos CODE_SIGN_IDENTITY="$code_sign_identity" PROVISIONING_PROFILE="$provisioning_profile" &> "$build_dir/build_device_debug.log"
+xcodebuild clean build -configuration "Debug" -sdk iphoneos CODE_SIGN_IDENTITY="$CODE_SIGNING_IDENTITY" PROVISIONING_PROFILE="$PROVISIONING_PROFILE" &> "$build_dir/build_device_debug.log"
 if [ $? -ne "0" ]; then
     echo "[STATUS] Build failed"
     echo ""

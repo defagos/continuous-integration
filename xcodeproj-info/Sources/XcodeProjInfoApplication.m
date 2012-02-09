b5405ab00a1c0f692de08bd716a939a9d6308160
Samuel Défago <defagos@gmail.com>
2012-02-09 09:45:17 +0100

    Added usage information

--- xcodeproj-info/Sources/XcodeProjInfoApplication.m
+++ xcodeproj-info/Sources/XcodeProjInfoApplication.m
@@ -8,6 +8,15 @@
 
 #import "XcodeProjInfoApplication.h"
 
+static NSString * const kApplicationVersion = @"1.0";
+
+@interface XcodeProjInfoApplication ()
+
+- (void)displayHelpForApplication:(DDCliApplication *)application;
+- (void)displayVersionForApplication:(DDCliApplication *)application;
+
+@end
+
 @implementation XcodeProjInfoApplication
 
 #pragma mark Object creation and destruction
@@ -29,6 +38,30 @@
 
 #pragma mark Application
 
+- (void)displayHelpForApplication:(DDCliApplication *)application
+{
+    ddprintf(@"Extract information from an Xcode project. Currently only configuration\n"
+             "information is returned, but other options may appear in the future.\n"
+             "\n"
+             "The command locates projects in the directory it is run from. If several\n"
+             "projects are found, the command exits. Use -p to select the project you\n"
+             "want to load in such cases case\n"
+             "\n"
+             "Usage: %@ [-p project_name][-v] [-h]\n"
+             "\n"
+             "Options:\n"
+             "   -h:                    Display this documentation\n"
+             "   -p:                    If you have multiple projects in the same\n"
+             "                          directory, indicate which one must be used using\n"
+             "                          this option (without the .xcodeproj extension)\n"
+             "   -v:                    Print the script version number\n", [application name]);
+}
+
+- (void)displayVersionForApplication:(DDCliApplication *)application
+{
+    ddprintf(@"%@ version %@", [application name], kApplicationVersion);
+}
+
 - (void)application:(DDCliApplication *)application willParseOptions:(DDGetoptLongParser *)optionsParser
 {
 	DDGetoptOption optionTable[] = {
@@ -43,8 +76,19 @@
 
 - (int)application:(DDCliApplication *)application runWithArguments:(NSArray *)arguments
 {
-    // TODO:
-    return 0;
+    if (self.version) {
+        [self displayVersionForApplication:application];
+        return EX_OK;
+    }
+    
+    if (self.help) {
+        [self displayHelpForApplication:application];
+        return EX_USAGE;
+    }
+    
+    
+    [self displayHelpForApplication:application];
+    return EX_USAGE;
 }
 
 @end

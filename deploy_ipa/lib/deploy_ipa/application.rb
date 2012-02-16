daca69cfe275bdfbf93606bdb5f69240d2e61cec
Samuel Défago <defagos@gmail.com>
2012-02-16 07:04:30 +0100

    Nesting classes

--- deploy_ipa/lib/deploy_ipa/application.rb
+++ deploy_ipa/lib/deploy_ipa/application.rb
@@ -1,9 +1,26 @@
-require 'Target'
-
 class Application
   attr_accessor :name
   attr_accessor :repositoryURL
   attr_accessor :targets
+  
+  class Target
+    attr_accessor :storeName
+    attr_accessor :identityName
+
+    def initialize(data, configurationFile)
+      @storeName = data['store_name']
+      @identityName = data['identity_name']
+      @configurationFile = configurationFile
+    end
+
+    def store
+      return @configurationFile.store(@storeName)
+    end
+
+    def identity
+      return @configurationFile.identity(@identityName)
+    end
+  end
   
   def initialize(data, configurationFile)
     @name = data['name']
